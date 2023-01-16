9e73b196b4dd218fa3aca9f676c0d5cca053e87a
lilianjiw107 <lilianjiw107@gmail.com>
2023-01-15 23:25:06 -0800

    Added comments

--- main.py
+++ main.py
@@ -11,18 +11,22 @@ parser.add_argument("-g", "--git-dir", help="local directory in which to assess 
 args = parser.parse_args()
 
 def check_status():
+    # Run command `git status`
     output = subprocess.check_output(['git', 'status'], cwd=args.git_dir).decode()
     lines = output.split('\n')
 
+    # Get current branch
     assert lines[0].startswith('On branch')
     branch_name = lines[0].removeprefix('On branch ')
     print('active branch: ' + branch_name)
     
+    # Check whether repository files have been modified
     changed = 'nothing to commit, working tree clean' != lines[-2]
     print('local changes: '+ str(changed))
 
 
 def check_head():
+    # Run command `git show HEAD`
     output = subprocess.check_output(['git', 'show', 'HEAD'], cwd=args.git_dir).decode()
     lines = output.split('\n')
 
@@ -30,12 +34,14 @@ def check_head():
     assert author_line.startswith('Author: ')
     assert date_line.startswith('Date:')
 
+    # Check whether the current head commit was authored in the last week
     time_string = date_line.removeprefix('Date:')[:-5].strip()
     dt = datetime.strptime(time_string, "%a %b %d %H:%M:%S %Y")
     dt_now = datetime.now()
     is_recent = (dt_now - dt).days <= 7
     print('recent commit: ' + str(is_recent))
 
+    # Check whether the current head commit was authored by Rufus
     author_name = author_line.removeprefix('Author:').strip().split(' ')[0]
     print('blame Rufus: ' + str(author_name == 'Rufus'))
 
