49f2319fc4f827b5b18f9cacd19989361ad3500a
lilianjiw107 <lilianjiw107@gmail.com>
2023-01-15 21:38:09 -0800

    Finished basic tasks

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# was-it-rufus
+# Was-it-rufus
 Coding take-home for Pronto.ai
+## How to run
+Run the following command in your terminal
+```
+python main.py -g GIT_DIR
+```
+where `GIT_DIR` is a local git repository directory.
+
+The program should print something like
+```
+active branch: main
+local changes: False
+recent commit: True
+blame Rufus: True
+```
+or
+```
+Not a git repository.
+```
+if the `GIT_DIR` is not a directory of a git repository.
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import argparse
 import subprocess
 
@@ -9,9 +10,40 @@ parser.add_argument("-g", "--git-dir", help="local directory in which to assess 
 
 args = parser.parse_args()
 
+def check_status():
+    output = subprocess.check_output(['git', 'status'], cwd=args.git_dir).decode()
+    lines = output.split('\n')
+
+    assert lines[0].startswith('On branch')
+    branch_name = lines[0].removeprefix('On branch ')
+    print('active branch: ' + branch_name)
+    
+    changed = 'nothing to commit, working tree clean' != lines[-2]
+    print('local changes: '+ str(changed))
+
+
+def check_head():
+    output = subprocess.check_output(['git', 'show', 'HEAD'], cwd=args.git_dir).decode()
+    lines = output.split('\n')
+
+    author_line, date_line = lines[1], lines[2]
+    assert author_line.startswith('Author: ')
+    assert date_line.startswith('Date:')
+
+    time_string = date_line.removeprefix('Date:')[:-5].strip()
+    dt = datetime.strptime(time_string, "%a %b %d %H:%M:%S %Y")
+    dt_now = datetime.now()
+    is_recent = (dt_now - dt).days <= 7
+    print('recent commit: ' + str(is_recent))
+
+    author_name = author_line.removeprefix('Author:').strip().split(' ')[0]
+    print('blame Rufus: ' + str(author_name == 'Rufus'))
+
 if __name__ == "__main__":
+    try:
+        check_status()
+        check_head()
 
-    output = subprocess.check_output(['git', 'status'], cwd=args.git_dir)
-
-    print(output)
-
+    except Exception as e:
+        # print(e)
+        print('Not a git repository.')
